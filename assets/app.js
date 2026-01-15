dc6793c53e56e09a2c6c8f485d4390861fc5ea5d
apoyomedicoips <apoyomedicoips@gmail.com>
2026-01-15 11:48:01 -0300

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,6 @@
 // Configuración general del frontend
 const CONFIG = {
-  GAS_ENDPOINT: "https://script.google.com/macros/s/AKfycbyB5pP12OUtEJJf8q3DiQPE5YSHR3TN34BwcFzM8JFFQ2DlmavNFc4SYwYSVwXhm2Xg7w/exec",
+  GAS_ENDPOINT: "https://script.google.com/macros/s/AKfycbzp5Hhs1luoHKtcMbv6uuug2nueZ7Xna8iPymIRV78q0i19CP75C20J_sj2go_2aUn2xw/exec",
   LOGO_URL: "https://i.postimg.cc/nLMVw5hz/logo-IPS.jpg", // sustituir por URL real del logo
   UMBRAL_CRITICO_DEFAULT: 7
 };
